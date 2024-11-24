fec11e021fb0ba62e128b51f5290029aaf0590df
amal velloth <amalv414@gmail.com>
2024-11-24 14:53:33 +0530

    svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <nav className='h-16 flex items-center justify-center w-full bg-transparent absolute z-10'>
-      <h1 className='text-3xl text-center font-bold'>Fit+</h1>
+      <h1 className='text-3xl text-center font-bold max-md:text-2xl'>Fit+</h1>
     </nav>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import joggingSvg from '../assets/jogging-bro.svg'
 
 function Home() {
     return (
         <>
             <section className='flex relative overflow-hidden'>
                 <div className="flex col h-screen w-[60%] max-md:w-[70%] justify-center items-center bg-white">
+
                     <div className="flex-col p-8 max-md:p-2">
+                        <img src={joggingSvg} className='max-w-[500px]' alt="" />
                         <div className="text">
                             <h1 className='text-black font-extrabold text-5xl max-md:text-3xl max-md:text-[calc(40px+0.5vw)]'>"MAKE YOUR FITNESS YOUR DAILY GOAL"</h1>
                         </div>
@@ -14,6 +17,7 @@ function Home() {
                             <button className='rounded-3xl my-5 ms-2 border border-black py-2 sm:px-10 max-sm:px-4 font-bold'>Sign Up</button>
                         </div>
                     </div>
+
                 </div>
                 <div className="flex items-center h-screen w-[40%] max-md:w-[30%] bg-[#BAFF00] relative overflow-hidden">
                     <div className=" w-full rotate-90">
