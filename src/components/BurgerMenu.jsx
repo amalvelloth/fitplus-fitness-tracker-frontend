8df14be74811b948a388e3307525921864673b74
amal velloth <amalv414@gmail.com>
2024-11-26 02:22:01 +0530

    update menu bar

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 function BurgerMenu() {
     const [isOpen, setIsOpen] = useState(false);
@@ -19,39 +20,72 @@ function BurgerMenu() {
                 <div className="w-12 h-12 cursor-pointer flex flex-col items-center justify-center">
                     <input className="hidden peer" type="checkbox" checked={isOpen} onChange={toggleMenu} />
                     <div
-                        className={`w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] ${isOpen ? '-rotate-45' : ''
+                        className={`w-[70%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.5rem] ${isOpen ? '-rotate-45' : ''
                             }`}
                     ></div>
                     <div
-                        className={`w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center ${isOpen ? 'hidden' : ''
+                        className={`w-[70%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center ${isOpen ? 'hidden' : ''
                             }`}
                     ></div>
                     <div
-                        className={`w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] ${isOpen ? 'rotate-45' : ''
+                        className={`w-[70%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.5rem] ${isOpen ? 'rotate-45' : ''
                             }`}
                     ></div>
                 </div>
             </label>
+
+
             <div
                 className={`absolute h-screen flex items-center p-6 top-0 left-0  bg-[#BAFF00] z-10 transition-transform duration-500 ${isOpen ? 'translate-x-0' : '-translate-x-full'
                     }`}
             >
                 <ul className='flex flex-col items-start justify-center h-full space-y-8 px-6'>
                     <li>
-                        <a href="#" className='text-lg font-semibold text-black'>Dashboard</a>
+                        <NavLink
+                            to="/dashboard"
+                            className="text-lg font-bold text-black"
+                            onClick={() => setIsOpen(false)}
+                            activeClassName="underline"
+                        >
+                            Dashboard
+                        </NavLink>                    </li>
+                    <li>
+                        <NavLink
+                            to="/taskcontrol"
+                            className="text-lg font-bold text-black"
+                            onClick={() => setIsOpen(false)}
+                            activeClassName="underline"
+                        >
+                            Task Control
+                        </NavLink>                    </li>
+                    <li>
+                        <NavLink
+                            to="/trainingprograms"
+                            className="text-lg font-bold text-black"
+                            onClick={() => setIsOpen(false)}
+                            activeClassName="underline"
+                        >
+                            Training Programs
+                        </NavLink>
                     </li>
                     <li>
-                        <a href="#" className='text-lg font-semibold text-black'>Task Control</a>
-                    </li>
+                        <NavLink
+                            to="/settings"
+                            className="text-lg font-bold text-black"
+                            onClick={() => setIsOpen(false)}
+                            activeClassName="underline"
+                        >
+                            Settings
+                        </NavLink>                    </li>
                     <li>
-                        <a href="#" className='text-lg font-semibold text-black'>Training Programs</a>
-                    </li>
-                    <li>
-                        <a href="#" className='text-lg font-semibold text-black'>Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" className='text-lg font-semibold text-black'>Sign Out</a>
-                    </li>
+                        <NavLink
+                            to="/signout"
+                            className="text-lg font-bold text-black"
+                            onClick={() => setIsOpen(false)}
+                            activeClassName="underline"
+                        >
+                            Sign Out
+                        </NavLink>                    </li>
                 </ul>
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar({ className }) {
   return (
     <nav className={`h-16 flex items-center justify-center w-full absolute z-10 ${className}`}>
       <BurgerMenu/>
-      <h1 className='text-3xl font-bold max-md:text-2xl'>Fit+</h1>
+      <h1 className='text-3xl font-bold max-md:text-2xl z-50'>Fit+</h1>
     </nav>
   )
 }
