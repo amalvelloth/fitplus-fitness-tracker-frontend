e109c079f365a1d10bc4b571aee4be649bee0125
amal velloth <amalv414@gmail.com>
2024-11-28 00:36:11 +0530

    added draggable todo

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,15 +6,15 @@ function Dashboard() {
     <>
       <section className="flex">
         <DesktopMenu />
-        <div className="h-screen w-full relative flex items-center bg-[#27282C]">
-          <div className="flex items-center justify-start px-8 max-sm:px-4">
+        <div className="h-screen w-full relative flex  bg-[#27282C]">
+          <div className="flex items-start pt-24 justify-start px-8 max-sm:px-4">
             <div className="greetings">
               <h1 className="text-white text-2xl max-sm:text-lg">
                 Good morning <span className="font-extrabold text-4xl max-sm:text-2xl">AMAL V</span>
               </h1>
               <h2 className="text-white text-4xl max-sm:text-3xl font-semibold mt-2">Welcome back! 🎉</h2>
             </div>
-            <div className="avatar bg-white w-24 h-24 rounded-full overflow-hidden">
+            <div className="avatar bg-white w-24 h-24 rounded-full border-none overflow-hidden">
               <img
                 className="object-cover w-full h-full"
                 src="https://i.pinimg.com/736x/56/28/a6/5628a66967df6318515fcbe7e912e274--cr-ronaldo-cristiano-ronaldo.jpg"
