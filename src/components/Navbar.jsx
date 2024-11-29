172de3e73189d8488cc567bd711b9ff2c7a58721
amal velloth <amalv414@gmail.com>
2024-11-29 16:44:13 +0530

    href /

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,10 @@ import BurgerMenu from './BurgerMenu'
 function Navbar({ className }) {
   return (
     <nav className={`h-16 flex items-center justify-center w-full absolute z-10 ${className}`}>
-      <BurgerMenu/>
-      <h1 className='text-3xl font-bold max-md:text-2xl z-50'>Fit+</h1>
+      <BurgerMenu />
+      <a href="/">
+        <h1 className='text-3xl font-bold max-md:text-2xl z-50'>Fit+</h1>
+      </a>
     </nav>
   )
 }
